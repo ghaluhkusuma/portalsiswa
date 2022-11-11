93996968ab25edfd5c9730f5f9ed8870d335a77f
Maysanira <indahermerah@gmail.com>
2022-11-11 18:51:00 +0700

    Menambahkan foto di data guru

--- dataguru.php
+++ dataguru.php
@@ -4,35 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Guru</title>
 </head>
 <body>
-<div class="container">
-    <div class="header-project">
-        <h3 class="project-title">Latest Project</h3>
-        <a href="#" class="project-link">See All"</a>
-    </div>
-    <div class="content-project">
-        <section>
-            <img class="project-img" src="/src/img/Ghaluh.png" alt="">
-            <h5 class="project-titles" >Ghaluh Kusumaningrum</h5>
-            <!-- < href="#" class="project-links"  >See Detail</a> -->
-    
-        </section>
-        <section>
-            <img class="project-img" src="src/img/image-2.svg" alt="">
-            <h5 class="project-titles">Finance Mobile App</h5>
-            <a href="#" class="project-link">See Detail</a>
-        </section>
-        <section>
-            <img class="project-img" src="src/img/image-3.svg" alt="">
-            <h5 class="project-titles">Your Mobile App</h5>
-            <a href="#" class="project-link">See Detail</a>
-        </section>
-        <section>
-            <img class="project-img" src="src/img/image-4.svg" alt="">
-            <h5 class="project-titles">Dashboard Mobile App</h5>
-            <a href="#" class="project-link">See Detail</a>
-        </section> 
+    <!-- Our Great Team -->
+    <div class="bg-cream">
+<h5 class="title-cream">Rekayasa Perangkat Lunak</h5>
+<div class="content-team">
+<section>
+    <img class="project-img" src="src/img/Bu Endah.png" alt="">
+    <h5 class="project-titles">Endah Emera</h5>
+    <p class="team-link">Produk Kreatif</p>
+</section>
+<section>
+    <img class="project-img" src="src/img/Ghaluh.png" alt="">
+    <h5 class="project-titles">Ghaluh Kusumaningrum</h5>
+    <p class="team-link">Informatika</p>
+</section>
+<section>
+    <img class="project-img" src="src/img/Ahmad Bagus.png" alt="">
+    <h5 class="project-titles">Ahmad Bagus Pambudi</h5>
+    <p class="team-link">Basis Data</p>
+</section>
+<section>
+    <img class="project-img" src="src/img/Mas Imut.png" alt="">
+    <h5 class="project-titles">Imam Muthohar</h5>
+    <p class="team-link">Pemrograman WEB</p>
+</section>
+</div>
+
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -9,8 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-
-    
+<h4 class="content-tit mb-0 mt-4">Portal Informasi Siswa<br><br></h4>
+<nav class="navbar navbar-expand-lg"></nav> 
      <!-- Content header -->
        <div class="container px-5 mt-5">
          <div class="row align-items-lg-center">
@@ -18,8 +18,8 @@
              <div class="d-flex flex-column"  >
              <h3 class="content-title">Selamat Datang<br><br></h3>
              <p class="content-subtitle">Kurikulum XSATA ingin memberikan pelayanan terbaik berkaitan belajar mengajar di SMKN 1 Tengaran. Berikut ini Web App Kurikulum yang dapat diakses!</p>
-             <button class="btn btn-primary" type="submit">Data Siswa</button><br>
-              <button class="btn btn-primary" type="submit">Data Guru</button> 
+             <button class="btn btn-primary w-25" type="submit">Data Siswa</button><br>
+              <button class="btn btn-primary w-25" type="submit">Data Guru</button> 
         
            </div>
            </div>
@@ -28,37 +28,15 @@
            </div>
           </div>
          </div>
-
- <!-- Benefits -->
-         <!-- <div class="container px-5 mt-10">
-           <div class="row gy-5 gx-md-5">
-             <div class="col-12 col-md-3">
-               <div class="text-center">
-                 <span class="text-benefit">Aplikasi Investasi</span>
-             <h class="title-benefit">Terpopuler</h5>
-             <p class="subtitle-benefit d-flex align-items-center justify-content-center">Berdasarkan CNN Media <a href="#" class="ms-1"><svg style="width: 1rem;" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>
-             </svg>
-             </a>
-           </p> 
-               </div>
-             </div>
-             <div class="col-12 col-md-3">
-               <div class="text-center">
-               <span class="text-benefit">Advisor Terbaik</span>
-               <h5 class="title-benefit">Mr Advise</h5>
-               <p class="subtitle-benefit">A.I Investasi terakurat</p>    
-             </div>
-             </div> -->
-         
-      
+     
          </div>
          <!-- untuk membuat garis -->
          <nav class="navbar navbar-expand-lg"></nav>    
  <!-- Footer -->
  <div class="border-top py-4 mt-10">
    <div class="container px-5 d-flex flex-lg-row flex-column justify-content-between">
-     <img class="d-lg-block d-none" src="portalsiswa/src/img/Upo-Djiwo.png" alt="">
-     <img class="d-lg-none d-block mb-4" style="width: 8rem;" src="portalsiswa/src/img/Upo-Djiwo.png" alt="">
+     <img class="d-lg-block d-none" src="src/img/UpoDjiwo.png" alt="">
+     <img class="d-lg-none d-block mb-4" style="width: 2rem;" src="src/img/UpoDjiwo.png" alt="">
    <div class="d-flex flex-lg-row flex-column">
      <a href="#" class="me-3 text-decoration-none text-secondary">Created by our team</a>
     
