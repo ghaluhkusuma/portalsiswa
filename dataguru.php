d1cd13f6609f22f19f13d3d0bc68d72c73a55cec
Maysanira <indahermerah@gmail.com>
2022-11-11 13:57:41 +0700

    Menambahkan homepage awal

---
+++ dataguru.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div class="header-project">
+        <h3 class="project-title">Latest Project</h3>
+        <a href="#" class="project-link">See All"</a>
+    </div>
+    <div class="content-project">
+        <section>
+            <img class="project-img" src="/src/img/Ghaluh.png" alt="">
+            <h5 class="project-titles" >Ghaluh Kusumaningrum</h5>
+            <!-- < href="#" class="project-links"  >See Detail</a> -->
+    
+        </section>
+        <section>
+            <img class="project-img" src="src/img/image-2.svg" alt="">
+            <h5 class="project-titles">Finance Mobile App</h5>
+            <a href="#" class="project-link">See Detail</a>
+        </section>
+        <section>
+            <img class="project-img" src="src/img/image-3.svg" alt="">
+            <h5 class="project-titles">Your Mobile App</h5>
+            <a href="#" class="project-link">See Detail</a>
+        </section>
+        <section>
+            <img class="project-img" src="src/img/image-4.svg" alt="">
+            <h5 class="project-titles">Dashboard Mobile App</h5>
+            <a href="#" class="project-link">See Detail</a>
+        </section> 
+</body>
+</html>
